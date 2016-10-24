afd2e6a0724ea4a6b6cda7101b7b372c27dd8388
Niels Kleizen <niels.kleizen@outlook.com>
2016-10-24 20:08:09 +0000

    new startup project

---
+++ Code/Avocado.Startup/Avocado.Startup.cpp
@@ -0,0 +1,21 @@
+// Avocado.Startup.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "../avocado.game/Game.h"
+
+
+int main()
+{
+	GameContext context;
+	GameTexture blackTexture;
+	GameTexture whiteTexture;
+	TileFactory tileFactory = TileFactory(blackTexture, whiteTexture, context);
+	Board board(tileFactory, 500, 500);
+	Game game(board, context);
+
+	game.Run();
+
+	return 0;
+}
+
